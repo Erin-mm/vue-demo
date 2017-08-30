b02066bd1a4b2606c3234f7a81a98b64e376be42
Erin <erin_mm@163.com>
2017-08-30 18:05:26 +0800

    测试

--- src/components/test/test.vue
+++ src/components/test/test.vue
@@ -2,12 +2,15 @@
   <div class="test">
     <ul>
       <li><router-link :to="{name:'watchTest'}">watch</router-link></li>
+      <li><router-link :to="{name:'globalWatcher'}">全局监听方法</router-link></li>
     </ul>
     <router-view></router-view>
   </div>
 </template>
 <script>
-	export default{}
+	export default{
+
+  }
 </script>
 <style lang="scss" rel="stylesheet/scss" scoped>
   .test{
